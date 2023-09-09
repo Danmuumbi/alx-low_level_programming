02a23d837523a5c761bbb2098140ae61589ed336
Danmuumbi <muuomuumbi@gmail.com>
2023-09-09 11:54:14 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,8 +13,9 @@
 int main(void)
 
 {
-	
+
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -25,7 +26,7 @@ int main(void)
 
 	}
 
-	
+
 	else if (n == 0)
 
 	{
@@ -33,13 +34,13 @@ int main(void)
 		printf("%d is zero\n", n);
 
 	}
-	
+
 	else
 
 	{
 
 		printf("%d is negative\n", n);
-	
+
 	}
 
 	return (0);
