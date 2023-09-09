5b49846e8fb0436a4262943b2cf8a92a1eb9ecde
Danmuumbi <muuomuumbi@gmail.com>
2023-09-08 22:02:09 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,13 @@
  * main -  Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 
 {
 
 	int d;
-	for (d ='0'; d <= '9'; d++)
+	for (d = '0'; d <= '9'; d++)
 	{
 		putchar(d);
 		if (d != '9')
