7eceaf2bb8901cc2f25abd93fe372dfb2f683799
Danmuumbi <muuomuumbi@gmail.com>
2023-09-10 05:47:03 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -11,16 +13,16 @@ int main(void)
 
 	int d, p;
 
-	for (d = 0; d <= 8; d++)
+	for (d = '0'; d <= 9; d++)
 	{
-		for (d = d + 1; d <= 9; d++)
+		for (p = d + 1; p <= 9; p++)
 		{
 			if (p != d)
 		{
 			putchar(d);
-	    putchar(p);
-	    if (d == '8' && p == '9')
-		    continue;
+			putchar(p);
+if (d == '8' && p == '9')
+continue;
 	    putchar(',');
 		putchar(' ');
 		}
