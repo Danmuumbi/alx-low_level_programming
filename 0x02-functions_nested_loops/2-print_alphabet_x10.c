681116762dd09bc09d03d8d8cd23abd522040133
Danmuumbi <muuomuumbi@gmail.com>
2023-09-13 13:00:01 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -14,10 +14,10 @@ void print_alphabet_x10(void)
 	while(i <= 10)
 
 	{
-		for (c = 'a'; c <= 'z'; c++);
-	}
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
 			_putchar('\n'); i++;
+}
 }
