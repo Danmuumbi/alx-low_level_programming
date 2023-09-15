7a869824c3a12343e345d3edbd5946d50894c7a6
Danmuumbi <muuomuumbi@gmail.com>
2023-09-14 22:13:35 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_line - Prints a straight line of '_' characters.
+* @n: The number of times to print '_'.
+*/
+
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+
+		_putchar('\n');
+	}
+}
+
