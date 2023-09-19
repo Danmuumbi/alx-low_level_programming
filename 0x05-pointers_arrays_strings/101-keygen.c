8dbe1fc54109f2208cb527a7c3916e936296094a
Danmuumbi <muuomuumbi@gmail.com>
2023-09-19 07:52:30 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,22 +6,24 @@
 
 int main(void)
 {
-	char password[PASSWORD_LENGTH + 1]; // +1 for the null terminator
+	char password[PASSWORD_LENGTH + 1];
 	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	int i;
 
-	srand(time(0)); // Seed the random number generator with the current time
+	srand(time(0));
 
 	for (i = 0; i < PASSWORD_LENGTH; i++)
 	{
-		int index = rand() % (sizeof(charset) - 1); // Generate a random index within the charset
+
+		int index = rand() % (sizeof(charset) - 1);
 		password[i] = charset[index];
+
 	}
 
-	password[PASSWORD_LENGTH] = '\0'; // Null-terminate the password string
+	password[PASSWORD_LENGTH] = '\0';
 
 	printf("%s\n", password);
 
-	return 0;
+	return (0);
 }
 
