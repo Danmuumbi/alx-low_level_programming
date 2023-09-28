fff3c243db83ec80c0b5754621439afee7c24b6f
Danmuumbi <muuomuumbi@gmail.com>
2023-09-28 09:30:15 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,35 +1,31 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - calculates the natural square root of a number
- * @n: the number for which to calculate the square root
- *
- * Return: the square root of n, or -1 if n does not have a natural square root
- */
-int _sqrt_recursion(int n)
+* _sqrt - Helper function to find the square root using recursion
+* @n: Number to find the square root of
+* @guess: Current guess for the square root
+*
+* Return: Square root of n if found, -1 if not found
+*/
+int _sqrt(int n, int guess)
 {
-    if (n < 0)
-        return -1;
-    else if (n == 0 || n == 1)
-        return n;
-    else
-        return _sqrt_helper(n, 1);
+	if (guess * guess == n)
+		return (guess);
+	if (guess * guess > n)
+		return (-1);
+	return (_sqrt(n, guess + 1));
 }
 
 /**
- * _sqrt_helper - helper function to calculate square root using recursion
- * @n: the number for which to calculate the square root
- * @guess: the current guess for the square root
- *
- * Return: the square root of n, or -1 if n does not have a natural square root
- */
-int _sqrt_helper(int n, int guess)
+* _sqrt_recursion - Returns the natural square root of a number
+* @n: Number to find the square root of
+*
+* Return: Square root of n if found, -1 if not found
+*/
+int _sqrt_recursion(int n)
 {
-    if (guess * guess == n)
-        return guess;
-    else if (guess * guess > n)
-        return -1;
-    else
-        return _sqrt_helper(n, guess + 1);
+	if (n < 0)
+		return (-1);
+	return (_sqrt(n, 0));
 }
 
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,30 @@
 #include "main.h"
 
 /**
- * is_prime_number - checks if a number is prime
- * @n: the integer to check
- *
- * Return: 1 if n is prime, 0 otherwise
- */
+* is_prime_number - checks if a number is prime
+* @n: the integer to check
+*
+* Return: 1 if n is prime, 0 otherwise
+*/
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return 0; // 0 and 1 are not prime numbers
-    return is_prime_helper(n, 2);
+	if (n <= 1)
+		return (0);
+	return (is_prime_number(n, 2));
 }
 
 /**
- * is_prime_helper - helper function to check if a number is prime
- * @n: the integer to check
- * @divisor: the current divisor to check
- *
- * Return: 1 if n is prime, 0 otherwise
- */
-int is_prime_helper(int n, int divisor)
+* is_prime_helper - helper function to check if a number is prime
+* @n: the integer to check
+* @divisor: the current divisor to check
+*
+* Return: 1 if n is prime, 0 otherwise
+*/
+int is_prime_number(int n, int divisor)
 {
-    if (divisor >= n)
-        return 1; // It's prime if no divisor is found
-    if (n % divisor == 0)
-        return 0; // Found a divisor other than 1 and itself
-    return is_prime_helper(n, divisor + 1);
+	if (divisor >= n)
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (is_prime_number(n, divisor + 1));
 }
-
