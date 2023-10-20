d910ca58284d84b51766a082eac81ec5ffaff02c
Danmuumbi <muuomuumbi@gmail.com>
2023-10-20 00:18:04 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,31 +11,30 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    char *str_copy;
-    unsigned int str_len = 0;
+	list_t *new_node;
+	char *str_copy;
+	unsigned int str_len = 0;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return (NULL);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    str_copy = strdup(str);
-    if (str_copy == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	str_copy = strdup(str);
+	if (str_copy == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    while (str[str_len])
-        str_len++;
+	while (str[str_len])
+		str_len++;
+	new_node->str = str_copy;
+	new_node->len = str_len;
+	new_node->next = *head;
+	*head = new_node;
 
-    new_node->str = str_copy;
-    new_node->len = str_len;
-    new_node->next = *head;
-    *head = new_node;
-
-    return (new_node);
+	return (new_node);
 }
