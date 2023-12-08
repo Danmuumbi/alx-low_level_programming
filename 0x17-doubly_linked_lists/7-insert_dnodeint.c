8f0218b768c552ac7eea7d4aed13b80cb4c06c60
Danmuumbi <muuomuumbi@gmail.com>
2023-12-08 09:09:31 -0800

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position in a doubly linked list.
+ * insert_dnodeint_at_index - Inserts,
+ * a new node at a given position in a doubly linked list.
  * @h: Pointer to a pointer to the head of the linked list.
  * @idx: Index where the new node should be added. Index starts at 0.
  * @n: Data for the new node.
@@ -12,13 +13,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *current = *h;
 	unsigned int i = 0;
+
 	if (h == NULL)
-		return NULL;
-	
+
+		return (NULL);
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
-		return NULL;
-
+		return (NULL);
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
@@ -29,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (*h != NULL)
 			(*h)->prev = new_node;
 		*h = new_node;
-		return new_node;
+		return (new_node);
 	}
 
 	while (current != NULL && i < idx - 1)
@@ -41,14 +43,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (current == NULL)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
-
 	new_node->next = current->next;
 	new_node->prev = current;
 	if (current->next != NULL)
 		current->next->prev = new_node;
 	current->next = new_node;
-
-	return new_node;
+	return (new_node);
 }
